1d350bd2f61b97d4a52fd70a165ab29fe0e86077
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-02-15 15:19:42 +0300

    44 ara donem

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,5 +13,26 @@ namespace Forms.Controllers
             return View(Repository.Products);
         }
 
+        [HttpGet]
+        public IActionResult Index(string searchString)
+        {
+            // products deðiþkenine tüm ürünler koyuluyor
+            var products = Repository.Products;
+
+            
+            // searchstring null veya boþ deðilse eðer bu koþul çalýþýyor
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                // Ürünlerin içinde arama yapýlýyor
+                // Mevcut ürünler arasýndan where koþuluyla linq expression kullanýlýyor
+                // p denilen deðiþken products içerisinde yer alan her bir ürünü temsil ediyor
+                // bu, products içerisindeki ürünlerin adlarý içerisinde, bizim search stringimiz var mý kontrol ediliyor
+                // varsa eðer, listeye çevirilerek, productsa kaydediliyor
+                products = products.Where(p => p.Name.ToLower().Contains(searchString)).ToList();
+            }
+            // bundan sonra tarayýcýda linkin sonuna ?searchString=iphone yazarak arama yapabiliriz
+
+            return View(products);
+        }
     }
 }
