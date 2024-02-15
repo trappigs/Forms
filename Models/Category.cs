f2c10fd1c96266745cd2601d55d72e4b92007e50
Muhammed Hatay <46846228+trappigs@users.noreply.github.com>
2024-02-15 14:58:56 +0300

    Forms/Product List/43

---
+++ Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace Forms.Models
+{
+    // kategori sınıfı
+    // kategorilerin id ve adını tutuyor
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        public string? Name { get; set; } = string.Empty;
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Forms.Models
+{
+    public class Product
+    {
+        // Product Id'nin namenin çağırılmasıyla tabloda nasıl görünmesi gerektiğini belirtiyoruz
+        [Display(Name = "Ürün Id")]
+        public int ProductId { get; set; }
+
+        [Display(Name = "Ürün Adı")]
+        public string? Name { get; set; } = string.Empty;
+
+        [Display(Name = "Fiyat")]
+        public int Price { get; set; }
+
+        [Display(Name = "Resim")]
+        public string Image { get; set; } = string.Empty;
+
+        [Display(Name = "Durum")]
+        public bool IsActive { get; set; }
+
+        [Display(Name = "Kategori")]
+        public int CategoryId { get; set; }
+    }
+}
